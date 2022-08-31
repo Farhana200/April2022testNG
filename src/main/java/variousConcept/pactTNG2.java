5d9dd42b0f370bf005c45ad902d51c1e4c48d37e
Farhana Rashid <farhanarashid31@gmail.com>
2022-08-31 03:25:48 -0500

    3rd commit

---
+++ src/main/java/variousConcept/pactTNG2.java
@@ -0,0 +1,21 @@
+package variousConcept;
+
+public class pactTNG2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+		test1();
+		test2();
+	}
+     public static void test1() {
+    	 System.out.println(1);
+    	 System.out.println(3);
+    	 System.out.println(12);
+     }
+     public static void test2(){
+    	 System.out.println(1);
+    	 System.out.println(3);
+    	 System.out.println(12);
+     }
+}
